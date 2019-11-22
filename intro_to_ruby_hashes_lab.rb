b25b19f7a0f0dcdc3978798cf46855c6096ca032
Jesse Tippit <jtippit223@gmail.com>
2019-11-22 06:06:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,13 @@
 def new_hash
-  # return an empty hash
+  hash1 {
+    
+  }
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  hash2 {
+    
+  }
 end
 
 def pioneer
