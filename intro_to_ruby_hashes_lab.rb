cdd63cc204f78019fdcbc62180eb460d59bc3a13
Jesse Tippit <jtippit223@gmail.com>
2019-11-22 06:17:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,8 +31,8 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  hash5 = {
-    hash
+  read_from_hash(hash) = {
+    :read_from_hash(key)
   }
   
 end
